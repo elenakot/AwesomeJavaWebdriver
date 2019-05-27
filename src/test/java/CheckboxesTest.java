9d8f4803213e092b66383588aa15e293bff6d49e
elenakot <eeremkina@gmail.com>
2019-05-26 23:47:29 -0400

    1

--- src/test/java/CheckboxesTest.java
+++ src/test/java/CheckboxesTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
@@ -11,6 +12,11 @@ import java.util.List;
 public class CheckboxesTest {
 
     WebDriver driver;
+    WebElement checkBox1;
+    WebElement checkBox2;
+    String attributeValue1;
+    String attributeValue2;
+
 
     @BeforeSuite
     public void beforeSuite(){
@@ -18,31 +24,48 @@ public class CheckboxesTest {
         driver = new FirefoxDriver();
     }
 
+    @AfterSuite
+    public void afterSuite() {
+        driver.close();
+    }
+
     @Test
     public void test001(){
         openCheckboxesPage();
+        findCheckboxesByTagnameInput();
+        defineStatusOfCheckboxes();
+        clickOnCheckbox1();
+        defineStatusOfCheckbox1();
+    }
 
-        List<WebElement> listOfInputs = driver.findElements(By.tagName("Input"));
-
-        int listSize = listOfInputs.size();
-
-        WebElement checkBox1 = listOfInputs.get(0);
-        WebElement checkBox2 = listOfInputs.get(1);
-
-        String attributeValue1 = checkBox1.getAttribute("checked");
-        String attributeValue2 = checkBox2.getAttribute("checked");
-
-        Assert.assertNull(attributeValue1);
-        Assert.assertNotNull(attributeValue2);
-
-        checkBox1.click();
-
+    private void defineStatusOfCheckbox1() {
         attributeValue1 = checkBox1.getAttribute("checked");
         Assert.assertNotNull(attributeValue1);
 
         String expectedAttributeValue = "true";
         Assert.assertEquals(expectedAttributeValue, attributeValue1);
 
+    }
+
+    private void clickOnCheckbox1() {
+        checkBox1.click();
+    }
+
+    private void defineStatusOfCheckboxes() {
+        attributeValue1 = checkBox1.getAttribute("checked");
+        attributeValue2 = checkBox2.getAttribute("checked");
+
+        Assert.assertNull(attributeValue1);
+        Assert.assertNotNull(attributeValue2);
+    }
+
+    private void findCheckboxesByTagnameInput() {
+        List<WebElement> listOfInputs = driver.findElements(By.tagName("Input"));
+
+        int listSize = listOfInputs.size();
+
+        checkBox1 = listOfInputs.get(0);
+        checkBox2 = listOfInputs.get(1);
     }
 
     private void openCheckboxesPage() {
