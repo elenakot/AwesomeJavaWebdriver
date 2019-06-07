ff253334984126bca04e39dc0480d7a58f5dbb8e
elenakot <eeremkina@gmail.com>
2019-06-07 10:16:35 -0400

    1

--- src/test/java/Xpath.java
+++ src/test/java/Xpath.java
@@ -1,2 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeSuite;
+
 public class Xpath {
+
+    WebDriver driver;
+
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.setProperty("webdriver.gecko.driver", "/Users/elenakot/IdeaProjects/AwesomeJavaWebdriver/src/test/resources/drivers/geckodriver 2");
+        driver = new FirefoxDriver(); }
+
+
 }
