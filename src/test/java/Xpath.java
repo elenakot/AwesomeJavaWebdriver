815cd459a500ec766e469a6aec21b1a3bfed16de
elenakot <eeremkina@gmail.com>
2019-06-10 10:59:42 -0400

    1

--- src/test/java/Xpath.java
+++ src/test/java/Xpath.java
@@ -1,6 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 public class Xpath {
 
@@ -12,5 +15,17 @@ public class Xpath {
         System.setProperty("webdriver.gecko.driver", "/Users/elenakot/IdeaProjects/AwesomeJavaWebdriver/src/test/resources/drivers/geckodriver 2");
         driver = new FirefoxDriver(); }
 
+
+    @Test
+    public void test001() {
+        String xpath1 = "";
+        WebElement element = driver.findElement(By.ByXPath(xpath1));
+
+        String adsXpath = "html/body/div[1]/div[2]/div[2]/div[1]/form/div[1]/div/div[1]/div/div/input[1]";
+
+        String relativeXpath = "//form/div[1]/div/div[1]/div/div/input[1]\n";
+
+
+    }
 
 }
